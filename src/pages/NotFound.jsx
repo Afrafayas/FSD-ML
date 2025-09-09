064857f244431ea24ea7b09d48b141ffd6228190
Afra Fayas <afrafayask@gmail.com>
2025-09-10 02:09:24 +0400

    Add assignment-11

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-96">
+      <h1 className="text-6xl font-bold text-red-500 mb-4">404 ❌</h1>
+      <p className="text-gray-600 text-lg">Oops! Page Not Found</p>
+    </div>
+  );
+}
+
+export default NotFound;
